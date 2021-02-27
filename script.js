d128a30599767d77ddef759164438053a77e4007
eumonti <eugeniomontinaro@gmail.com>
2021-02-27 11:31:52 +0100

    rewrite

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-var names = [];
-var picked = [];
+let input = document.getElementById("input");
+let output = document.getElementById("output");
+var names = input.value.split("\n");
 
 function pickRandom() {
-    let string = document.getElementById("input").value;
-    names = string.split("\n");
-    randomItem = names[Math.floor(Math.random() * names.length)];
-    if (picked.length !== names.length) {
-        if (!picked.includes(randomItem)) {
-            picked.push(randomItem);
-            console.log(randomItem);
-            document.getElementById("picked").value += randomItem + '\n';
-        } else {
-            pickRandom(names);
-        }
-    } else window.alert("Finito!");
+  if (!names.length) {
+    alert("Fine");
+    return;
+  }
+  let index = Math.floor(Math.random() * names.length);
+  output.value += names[index] + "\n";
+  names.splice(index, 1);
+  console.log(names);
 }
